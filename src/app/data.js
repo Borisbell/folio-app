dfebfdcfb1b3f6ff4313905bfb0d6ab13ad5958d
Boris Belov <jmorrison@bk.ru>
2023-04-17 00:49:53 +0300

    started main page

---
+++ src/app/data.js
@@ -0,0 +1,28 @@
+export const professionalData = {
+	experiences: [
+		{
+			role: "Senior product designer at ",
+            link: "https://uchi.ru/",
+            company_name: "UCHi.ru",
+            dates: "(2022 - present time)",
+		},
+        {
+			role: "Art director at ",
+            link: "https://botstore.cc/",
+            company_name: "Botobot",
+            dates: "(2021 - 2022)",
+		},
+        {
+			role: "Senior UI/UX Designer at",
+            link: "https://apps.apple.com/ru/app/%D0%BB%D0%B5%D0%BD%D1%82%D0%B0-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD-%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7-%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82%D0%BE%D0%B2/id1482112922",
+            company_name: "Lenta",
+            dates: "(2021 - 2021)",
+		},
+        {
+			role: "Senior UI/UX Designer at ",
+            link: "https://www.megafon.ru/",
+            company_name: "MegaFon",
+            dates: "(2017 – 2020)",
+		},
+	],
+};
